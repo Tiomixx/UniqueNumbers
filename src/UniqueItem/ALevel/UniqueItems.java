6a23515d2ab1d9c79bfc00caedbdfa41b38034f9
Tiomix <layeraz@gmail.com>
2019-12-18 15:25:18 +0200

    src dobavleno

---
+++ src/UniqueItem/ALevel/UniqueItems.java
@@ -0,0 +1,26 @@
+package UniqueItem.ALevel;
+
+public class UniqueItems {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 3, 1, 9, 4, 3, 8, 7};
+        for (int i = 1; i < array.length; i++) {
+            int ChisloSprava = array[i];
+            for (int j = i - 1; j >= 0; j--) {
+                int ChisloSleva = array[j];
+
+                if (ChisloSleva > ChisloSprava) {
+                    array[j + 1] = ChisloSleva;
+                    array[j] = ChisloSprava;
+                }
+            }
+        }
+
+        int UniqueItem = array.length;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] == array[i - 1]) {
+                UniqueItem = UniqueItem - 1;
+            }
+        }
+        System.out.print("Колличество уникальных чисел в массиве = " + UniqueItem);
+    }
+}
